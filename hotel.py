420b161043351a2819cff84ffdeb130659ab4eec
SPArrowX666 <mxu2492@gmail.com>
2025-07-11 10:31:08 +0900

    Update hotel.py

--- hotel.py
+++ hotel.py
@@ -30,8 +30,8 @@ def extract_price(text):
 df["Score"] = df["Like12"].apply(extract_score)
 df["Discount (%)"] = df["justifycenter"].apply(extract_discount)
 df["Price (Yen)"] = df["textgray80016"].apply(extract_price)
-hotel_names = df["タイトル"]
-
+df = df.rename(columns={"タイトル": "Hotel Name"})
+hotel_names = df["Hotel Name"]
 
 plt.figure(figsize=(12, 6))
 sns.barplot(x=df["Score"], y=hotel_names, palette="coolwarm")
